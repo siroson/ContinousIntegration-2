81f54db3bc899694fca37f58ed0f002d6dd73c8e
Siroson <siroson@f22labs.com>
2018-04-20 19:24:08 +0530

    Initial Commit

---
+++ ContInteg/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContInteg
+//
+//  Created by Siroson on 20/04/2018.
+//  Copyright © 2018 Siroson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
